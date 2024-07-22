f39ce381b1318bf9fb66d8d19d46917b218a00c7
iflyelf <81313124+iflyelf@users.noreply.github.com>
2024-07-22 10:18:26 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,18 +6,18 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+ // production
+  "docker.iflyelf.com": dockerHub,
+  "quay.iflyelf.com": "https://quay.io",
+  "gcr.iflyelf.com": "https://gcr.io",
+  "k8s-gcr.iflyelf.com": "https://k8s.gcr.io",
+  "k8s.iflyelf.com": "https://registry.k8s.io",
+  "ghcr.iflyelf.com": "https://ghcr.io",
+  "cloudsmith.iflyelf.com": "https://docker.cloudsmith.io",
+  "ecr.iflyelf.com": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.iflyelf.com": dockerHub,
 };
 
 function routeByHosts(host) {
